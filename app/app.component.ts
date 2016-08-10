f87a95c644fd465cc1a648088ee3dab87ab2cc5c
Dylan Barnard and Matt Carlson <dylan+matt@>
2016-08-10 10:07:08 -0700

    add custom event emitter to make selected kegs blue

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,8 @@ import { Component, EventEmitter } from 'angular2/core';
   inputs: ['kegList'],
   outputs: ['onKegSelect'],
   template: `
-  <h3 *ngFor="#currentKeg of kegList" (click)="kegClicked(currentKeg)">
+  <h3 *ngFor="#currentKeg of kegList" (click)="kegClicked(currentKeg)"
+    [class.selected]="currentKeg === selectedKeg">
     {{ currentKeg.name }}
   </h3>
   `
@@ -14,11 +15,13 @@ import { Component, EventEmitter } from 'angular2/core';
 export class KegListComponent {
   public kegList: Keg[];
   public onKegSelect: EventEmitter<Keg>;
+  public selectedKeg: Keg;
   constructor() {
     this.onKegSelect = new EventEmitter();
   }
   kegClicked(clickedKeg: Keg): void {
     console.log('child', clickedKeg);
+    this.selectedKeg = clickedKeg;
     this.onKegSelect.emit(clickedKeg);
   }
 }
